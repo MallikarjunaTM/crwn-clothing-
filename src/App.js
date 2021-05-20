94abe4874a78ba7e45ea6c9dacfd423c602eb01e
MallikarjunaTM <mallikarjuna271@gmail.com>
2021-05-20 13:16:30 +0530

    adding header with logo, shop contact

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import {Route,Switch} from 'react-router-dom';
 import ShopPage from './shop/shop.component';
+import './App.css';
+import Header from './header/header.component';
 
-const HatsPage = (props) =>{
-console.log(props);
-return <div><h1>Hats page</h1></div>
-};
+
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route exact path='/shop' component={ShopPage}/>
